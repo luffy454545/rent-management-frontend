e6db65b66cdac82e49eced73debd0c7cc544660c
luffy454545 <luffy4545454545@gmail.com>
2025-08-05 13:16:11 +0530

    setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import AppRoutes from "./routes/AppRoutes";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-
-    <RouterProvider router={AppRoutes} />
-
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // ✅ this line is critical!
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
